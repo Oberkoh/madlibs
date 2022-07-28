d9a7d91f0c753ac776ff91483e0d579e251e71bb
Michael Oberkoh <mcranskeneth@gmail.com>
2022-07-28 18:50:14 +0000

    Initial addition of files through upload
    feat: The primary file of the of the program is added

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+"""Prints a madlib string from user input
+
+input: 4 strings
+output: one formatted string which is contains user inputs
+"""
+
+adj = input("Enter Adjective: ")
+verb1 = input("Enter Verb: ")
+verb2 = input("Enter another Verb: ")
+famous_person = input("Enter a Famous Person's name: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because\
+ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
